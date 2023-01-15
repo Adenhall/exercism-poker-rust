a10708e8c34a3dc68bbbcd6ed9d4c8b146f42ad5
Adenhall <jamesadenhall@gmail.com>
2023-01-16 02:32:40 +0700

    Determine straight or flush

--- src/lib.rs
+++ src/lib.rs
@@ -15,12 +15,15 @@ enum HandRanking {
 
 struct Winners<'a> {
     rank: HandRanking,
-    hands: Vec<&'a str>
+    hands: Vec<&'a str>,
 }
 
 impl<'a> Winners<'a> {
     fn new() -> Self {
-        Winners { rank: HandRanking::HighCard, hands: vec![] }
+        Winners {
+            rank: HandRanking::HighCard,
+            hands: vec![],
+        }
     }
 
     fn add_new_winner(&mut self, hand: &'a str) {
@@ -34,7 +37,9 @@ impl<'a> Winners<'a> {
 }
 
 pub fn winning_hands<'a>(hands: &[&'a str]) -> Vec<&'a str> {
-    if hands.len() == 1 { return hands.to_vec() }
+    if hands.len() == 1 {
+        return hands.to_vec();
+    }
 
     let mut winners = Winners::new();
     for hand in hands {
@@ -44,14 +49,55 @@ pub fn winning_hands<'a>(hands: &[&'a str]) -> Vec<&'a str> {
         match prev_rank.cmp(&current_rank) {
             Ordering::Equal => winners.add_new_winner(hand),
             Ordering::Less => winners.renew_winners(hand),
-            _ => ()
+            _ => (),
         }
     }
-    
+
     winners.hands
 }
 
 fn determine_rank<'a>(hand: &'a str) -> HandRanking {
-    if hand.contains("3") { return HandRanking::StraightFlush }
-    HandRanking::FourOfAKind
+    // "3S 4S 5D 6H JH"
+
+    let (ranks, suits) = get_ranks_and_suits(hand);
+
+    if is_straight(ranks) & is_flush(suits) {
+        return HandRanking::StraightFlush;
+    }
+
+    HandRanking::HighCard
+}
+
+fn get_ranks_and_suits<'a>(hand: &'a str) -> (Vec<char>, Vec<char>) {
+    let cards = hand.split(" "); // [3S, 4S, ...]
+    let mut ranks: Vec<char> = vec![];
+    let mut suits: Vec<char> = vec![];
+    cards.for_each(|card| {
+        let mut chars = card.chars();
+        ranks.push(chars.next().unwrap());
+        suits.push(chars.next().unwrap());
+    });
+
+    (ranks, suits)
+}
+
+fn is_straight(ranks: Vec<char>) -> bool {
+    let order: Vec<char> = "A12345678910JQKA".chars().collect();
+
+    for straight in order.windows(5) {
+        let is_straight = ranks
+            .iter()
+            .all(|rank| straight.contains(rank));
+
+        if is_straight {
+            return true
+        }
+    }
+
+    false
+}
+
+fn is_flush(ranks: Vec<char>) -> bool {
+    let first = ranks[0];
+    ranks.iter().all(|rank| rank.eq(&first))
 }
