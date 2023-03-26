b916ba0f053d30e68beba24ccab31405eb25ddff
Adenhall <jamesadenhall@gmail.com>
2023-03-27 00:52:35 +0700

    Updates

--- src/lib.rs
+++ src/lib.rs
@@ -2,29 +2,29 @@ use std::cmp::Ordering;
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 enum HandRanking {
-    HighCard,
-    OnePair,
-    TwoPair,
-    ThreeOfAKind,
-    Straight,
-    Flush,
-    FullHouse,
-    FourOfAKind,
-    StraightFlush,
+    HighCard(i16),
+    OnePair(i16),
+    TwoPair(i16),
+    ThreeOfAKind(i16),
+    Straight(i16),
+    Flush(i16),
+    FullHouse(i16),
+    FourOfAKind(i16),
+    StraightFlush(i16),
 }
 
-const CARD_RANKING: &str = "A2345678910JQKA";
+const CARD_RANKING: [char; 14] = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
 
 struct Winners<'a> {
     rank: HandRanking,
-    hands: Vec<&'a str>,
+    hands: Vec<&'a str>
 }
 
 impl<'a> Winners<'a> {
     fn new() -> Self {
         Winners {
-            rank: HandRanking::HighCard,
-            hands: vec![],
+            rank: HandRanking::HighCard(0),
+            hands: vec![]
         }
     }
 
@@ -45,10 +45,10 @@ pub fn winning_hands<'a>(hands: &[&'a str]) -> Vec<&'a str> {
 
     let mut winners = Winners::new();
     for hand in hands {
-        let current_rank = determine_rank(hand);
-        let prev_rank = &winners.rank;
+        let next_rank = determine_rank(hand);
+        let current_rank = &winners.rank;
 
-        match prev_rank.cmp(&current_rank) {
+        match current_rank.cmp(&next_rank) {
             Ordering::Equal => winners.add_new_winner(hand),
             Ordering::Less => winners.renew_winners(hand),
             _ => (),
@@ -59,48 +59,53 @@ pub fn winning_hands<'a>(hands: &[&'a str]) -> Vec<&'a str> {
 }
 
 fn determine_rank<'a>(hand: &'a str) -> HandRanking {
-    // "3S 4S 5D 6H JH"
-
     let (ranks, suits) = get_ranks_and_suits(hand);
+    let score = ranks.chunks(2).fold(0, |_, pair| {
+
+        let first_idx = CARD_RANKING.iter().position(|x| x == pair.first().unwrap()).unwrap_or(0);
+        let second_idx = CARD_RANKING.iter().position(|x| x == pair.last().unwrap()).unwrap_or(0);
+
+        first_idx.max(second_idx) as i16
+    });
     let grouped_ranks = group_ranks(&ranks);
 
     if is_straight(&ranks) & is_flush(&suits) {
-        return HandRanking::StraightFlush;
+        return HandRanking::StraightFlush(score);
     }
 
     if let (4, _) = grouped_ranks {
-        return HandRanking::FourOfAKind;
+        return HandRanking::FourOfAKind(score);
     }
 
     if let (3, 2) = grouped_ranks {
-        return HandRanking::FullHouse;
+        return HandRanking::FullHouse(score);
     }
 
     if is_flush(&suits) {
-        return HandRanking::Flush;
+        return HandRanking::Flush(score);
     }
 
     if is_straight(&ranks) {
-        return HandRanking::Straight;
+        return HandRanking::Straight(score);
     }
 
     if let (3, _) = grouped_ranks {
-        return HandRanking::ThreeOfAKind;
+        return HandRanking::ThreeOfAKind(score);
     }
 
     if let (2, 2) = grouped_ranks {
-        return HandRanking::TwoPair;
+        return HandRanking::TwoPair(score);
     }
 
     if let (2, _) = grouped_ranks {
-        return HandRanking::OnePair;
+        return HandRanking::OnePair(score);
     }
 
-    HandRanking::HighCard
+    HandRanking::HighCard(score)
 }
 
 fn get_ranks_and_suits<'a>(hand: &'a str) -> (Vec<char>, Vec<char>) {
-    let cards = hand.split(" "); // [3S, 4S, ...]
+    let cards = hand.split(" ").map(|card| card.replace("10", "T"));
     let mut ranks: Vec<char> = vec![];
     let mut suits: Vec<char> = vec![];
     cards.for_each(|card| {
@@ -113,10 +118,13 @@ fn get_ranks_and_suits<'a>(hand: &'a str) -> (Vec<char>, Vec<char>) {
 }
 
 fn is_straight(ranks: &Vec<char>) -> bool {
-    let order: Vec<char> = CARD_RANKING.chars().collect();
+    for straight in CARD_RANKING.windows(5) {
+        let mut sorted_ranks = ranks.to_vec();
+        let mut sorted_straight = straight.to_vec();
+        sorted_ranks.sort();
+        sorted_straight.sort();
 
-    for straight in order.windows(5) {
-        if ranks.iter().all(|rank| straight.contains(rank)) {
+        if sorted_ranks == sorted_straight {
             return true;
         }
     }
@@ -130,7 +138,7 @@ fn is_flush(suits: &Vec<char>) -> bool {
 }
 
 fn group_ranks(ranks: &Vec<char>) -> (i16, i16) {
-    let mut cloned_ranks = ranks.clone();
+    let mut cloned_ranks = ranks.to_vec();
     cloned_ranks.sort();
     let mut result = (1, 0);
     for pair in cloned_ranks.windows(2) {
